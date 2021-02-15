62983ed073e54e7bdc281dd314cde296130fe67f
danielkhan-v1 <grigorovich314@gmail.com>
2021-02-15 21:48:45 +0200

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(array) {
+    if (array === undefined) return 0;
+    else if (!array.length) return 0;
 
-exports.min = function min (array) {
-  return 0;
-}
+    let minValue = array[0];
 
-exports.max = function max (array) {
-  return 0;
-}
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < minValue) {
+            minValue = array[i];
+        }
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return minValue;
+};
+
+exports.max = function max(array) {
+    if (array === undefined) return 0;
+    else if (!array.length) return 0;
+
+    let maxValue = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > maxValue) {
+            maxValue = array[i];
+        }
+    }
+
+    return maxValue;
+};
+
+exports.avg = function avg(array) {
+    if (array === undefined) return 0;
+    else if (!array.length) return 0;
+
+    return array.reduce((sum, current) => sum + current, 0) / array.length;
+};
